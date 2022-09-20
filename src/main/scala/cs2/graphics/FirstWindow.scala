2126a4ee7a0f339c44f2a93b8a7b7cf6a13b3678
Matt Hibbs <mhibbs@trinity.edu>
2022-09-20 13:55:46 -0500

    Continued graphics intro

--- src/main/scala/cs2/graphics/FirstWindow.scala
+++ src/main/scala/cs2/graphics/FirstWindow.scala
@@ -1,8 +1,11 @@
 package cs2.graphics
 
+import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.canvas.Canvas
+import scalafx.scene.paint.Color
+import scalafx.scene.input.MouseEvent
 
 object FirstWindow extends JFXApp {
   stage = new JFXApp.PrimaryStage {
@@ -12,7 +15,48 @@ object FirstWindow extends JFXApp {
       content = canvas
       val g = canvas.graphicsContext2D
 
-      g.fillRect(100,100, 300,200)
+      def cap(c:Double):Int = {
+        if(c < 0) 0
+        else if(c > 255) 255
+        else c.toInt
+      }
+
+      g.strokeRect(200,50, 300,200)
+      g.setStroke(Color.DodgerBlue)
+      g.setFill(Color.rgb(200,100,50))
+      g.setLineWidth(30)
+      g.strokeLine(0,0, 600,200)
+      g.fillOval(200,50, 300,200)
+      g.fillText("Hello, how are you?", 200,50)
+      
+      for(x <- 0 until 600) {
+        g.setStroke(Color.rgb(0, cap(x / 300.0 * 255), 255 - (x / 600.0 * 255).toInt))
+        g.strokeLine(x,0, x,400)
+      }
+      
+      g.setStroke(Color.Black)
+      g.setLineWidth(1)
+
+      def drawCirc(x:Double, y:Double, r:Double):Unit = {
+        g.strokeOval(x-r, y-r, r*2,r*2)
+        if(r > 5) {
+          drawCirc(x-r,y, r/2)
+          drawCirc(x+r,y, r/2)
+          drawCirc(x,y-r, r/2)
+          drawCirc(x,y+r, r/2)
+        }
+      }
+
+      drawCirc(300,200, 100)
+
+      g.setFill(Color.White)
+      g.fillRect(0,0, 600,400)
+
+      canvas.onMouseDragged = (e:MouseEvent) => {
+        g.strokeLine(300,200, e.x,e.y)
+      }
+
+
 
     }
   }
