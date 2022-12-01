bd006b68ce6f78f6c668b54e69c8aec9e5c8cbc7
Matt Hibbs <mhibbs@trinity.edu>
2022-12-01 13:49:43 -0600

    Heap based PriorityQueue

---
+++ src/main/scala/cs2/adt/HeapPriorityQueue.scala
@@ -0,0 +1,49 @@
+package cs2.adt
+
+class HeapPriorityQueue[A <% Ordered[A] : Manifest] extends PriorityQueue[A] {
+  private var heap = Array.ofDim[A](10)
+  private var len = 0
+
+  def add(elem:A):Unit = {
+    len += 1
+    if(len == heap.length) {
+      val tmp = Array.ofDim[A](len * 2)
+      for(i <- 0 until len) tmp(i) = heap(i)
+      heap = tmp
+    }
+    heap(len) = elem
+    var loc = len
+    while(loc > 1 && heap(loc) > heap(loc/2)) {
+      heap(loc) = heap(loc/2)
+      heap(loc/2) = elem
+      loc = loc/2
+    }
+  }
+  def get():A = {
+    val ret = heap(1)
+    heap(1) = heap(len)
+    len -= 1
+
+    var loc = 1
+    var keepGoing = true
+    while(loc*2 <= len && keepGoing) {
+      val bigKid = if(loc*2+1 > len || heap(loc*2) > heap(loc*2+1)) {
+          loc*2 }
+        else {
+          loc*2 + 1
+        }
+      if(heap(bigKid) > heap(loc)) {
+        val tmp = heap(loc)
+        heap(loc) = heap(bigKid)
+        heap(bigKid) = tmp
+        loc = bigKid
+      } else {
+        keepGoing = false
+      }
+    }
+
+    ret
+  }
+  def peek():A = heap(1)
+  def isEmpty():Boolean = { len == 0 }
+}
